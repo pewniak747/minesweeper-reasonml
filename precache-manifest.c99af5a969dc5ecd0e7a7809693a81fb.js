7d02588d48e9dc2cba9a9e25ff1c4b8304609602
Tomasz Pewiński <pewniak747@gmail.com>
2020-01-22 22:21:14 +0100

    Updates

---
+++ precache-manifest.c99af5a969dc5ecd0e7a7809693a81fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5568ccfdc17e0a47de7ab641202507ef",
+    "url": "/minesweeper-reasonml/index.html"
+  },
+  {
+    "revision": "bfd8b7f3901fb4b0c40f",
+    "url": "/minesweeper-reasonml/static/css/main.9c0bbada.chunk.css"
+  },
+  {
+    "revision": "15d81e1b5499b3a029ce",
+    "url": "/minesweeper-reasonml/static/js/2.ced77b94.chunk.js"
+  },
+  {
+    "revision": "bfd8b7f3901fb4b0c40f",
+    "url": "/minesweeper-reasonml/static/js/main.4c0551a6.chunk.js"
+  },
+  {
+    "revision": "1a292a0520a868c4ba1d",
+    "url": "/minesweeper-reasonml/static/js/runtime-main.6febfa83.js"
+  },
+  {
+    "revision": "e2342b05e19639d46eb7fb727b3ce8dd",
+    "url": "/minesweeper-reasonml/static/media/logo.e2342b05.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minesweeper-reasonml/precache-manifest.e447e77b974558f587a882d5f4064283.js"
+  "/minesweeper-reasonml/precache-manifest.c99af5a969dc5ecd0e7a7809693a81fb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/minesweeper-reasonml/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minesweeper-reasonml/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
